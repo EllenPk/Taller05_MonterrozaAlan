50ad9c0ee7dd5eadc1998294d45289a82986da80
AlanMonterroza <amonterr10@cuc.edu.co>
2024-09-19 11:25:40 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import milogo from './assets/img/why so serious.png'
+
+
+function App() {
+
+
+  return (
+   <>
+     <div>
+      <img src={milogo} alt="logo Joker" />
+      <h1>Why so serious???</h1>
+      <p>Welcome my little guy to the party</p>
+
+     </div>
+
+      
+    </>
+  )
+}
+
+export default App
